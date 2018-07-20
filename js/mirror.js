9d4ebdd3aa6164d99aa631f977db3c3b0121da58
Luka Milekic <lmilekic@gmail.com>
2018-07-20 09:47:03 -0400

    Added mirror code, removed dove-hawk code

---
+++ js/mirror.js
@@ -0,0 +1,31 @@
+//Set the page to auto reload (to update)
+setTimeout(function () {
+    location.reload();
+}, 10000);
+
+
+
+
+$(function () {
+    setInterval(updateClock, 500);
+    
+});
+
+
+
+
+
+function updateClock() {
+    
+    var curr = new Date();
+    var hours = curr.getHours();
+    var mins = curr.getMinutes();
+    var seconds = curr.getSeconds();
+    var pastnoon = false;
+    if (hours > 12) {
+        var hour = (hours - 12);
+        pastnoon = true;
+    }
+    var time = `${hours}:${mins} : ${seconds} ${pastnoon ? "PM" : "AM"}`
+    $(".clock").html(time);
+}
