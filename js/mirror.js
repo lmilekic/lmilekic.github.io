d2eaa70440496149e0d62cf3c098e838eb461fd6
Luka Milekic <lmilekic@gmail.com>
2018-07-20 10:07:24 -0400

    bigger text

--- js/mirror.js
+++ js/mirror.js
@@ -21,11 +21,14 @@ function updateClock() {
     var hours = curr.getHours();
     var mins = curr.getMinutes();
     var seconds = curr.getSeconds();
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
     var pastnoon = false;
     if (hours > 12) {
         var hour = (hours - 12);
         pastnoon = true;
     }
-    var time = `${hours}:${mins} : ${seconds} ${pastnoon ? "PM" : "AM"}`
+    var time = `${hours}:${mins}:${seconds} ${pastnoon ? "PM" : "AM"}`
     $(".clock").html(time);
 }
