d68085d9505e7549d9f5a8cf6772d6ace473ab1e
Luka Milekic <lmilekic@gmail.com>
2019-05-25 11:57:21 -0400

    prettied some stuff up, added basic temp

---
+++ js/clock.js
@@ -0,0 +1,23 @@
+// Handles making the clock, and making it pretty
+$(function () {
+    setInterval(updateClock, 500);
+    
+});
+
+function updateClock() {
+    
+    var curr = new Date();
+    var hours = curr.getHours();
+    var mins = curr.getMinutes();
+    var seconds = curr.getSeconds();
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
+    var pastnoon = false;
+    if (hours > 12) {
+        var hour = (hours - 12);
+        pastnoon = true;
+    }
+    var time = `${hours}:${mins}:${seconds} ${pastnoon ? "PM" : "AM"}`
+    $(".clock").html(time);
+}
--- js/mirror.js
+++ js/mirror.js
@@ -1,35 +1,22 @@
 //Set the page to auto reload (to update)
-// setTimeout(function () {
-//     location.reload();
-// }, 10000);
+setTimeout(function () {
+    refreshPage();
+}, 1000 * 60 * 60 * 12) // 12 hours
 
 
 
-document.body.requestFullscreen();
-
-$(function () {
-    setInterval(updateClock, 500);
-    
+document.getElementById('mainBody').addEventListener('click', () => {
+	if (screenfull.enabled) {
+		screenfull.request();
+	}
 });
 
-
-
-
-
-function updateClock() {
-    
-    var curr = new Date();
-    var hours = curr.getHours();
-    var mins = curr.getMinutes();
-    var seconds = curr.getSeconds();
-    if (mins < 10) {
-        mins = "0" + mins;
-    }
-    var pastnoon = false;
-    if (hours > 12) {
-        var hour = (hours - 12);
-        pastnoon = true;
-    }
-    var time = `${hours}:${mins}:${seconds} ${pastnoon ? "PM" : "AM"}`
-    $(".clock").html(time);
+function refreshPage() {
+    $.ajax({
+        url: "",
+        context: document.body,
+        success: function(s,x){
+            $(this).html(s);
+        }
+    });
 }
